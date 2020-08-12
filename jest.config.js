3835ea5b2157e8223731db12a92a6155af364af6
Ihsan Mujdeci <ihsan@upaged.com>
2020-08-12 16:42:39 +1000

    tests passing with aliasing

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig.json');
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ["build"]
+  testPathIgnorePatterns: ["build"],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' })
 };
